f6c70449207b1fcd5710aaa632fbaee47b5410e8
Jameson Cheong <jctheathlete@gmail.com>
2019-11-09 17:31:18 -0800

    created city and tour classes

---
+++ City.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jcheong on 2019-11-09.
+//
+
+#include "City.hpp"
---
+++ City.hpp
@@ -0,0 +1,14 @@
+//
+// Created by jcheong on 2019-11-09.
+//
+
+#ifndef GENETICALGORITHM_CITY_HPP
+#define GENETICALGORITHM_CITY_HPP
+class City{
+private:
+    double x;
+    double y;
+public:
+
+};
+#endif //GENETICALGORITHM_CITY_HPP
