758a1ee1654852dca55738282aeb6a29eac65721
Jameson Cheong <jctheathlete@gmail.com>
2019-11-11 21:53:16 -0800

    adding tour to github

---
+++ Tour.cpp
@@ -0,0 +1,130 @@
+//
+// Created by jcheong on 2019-11-09.
+//
+
+#include <random>
+#include <algorithm>
+#include "Tour.hpp"
+#include <cmath>
+#include <iostream>
+//instantiate random device
+random_device randDevice;
+
+//tour ID counter
+long Tour::tourIDCounter = 0;
+// instantiate random engine
+auto rn = default_random_engine{};
+
+//constructor
+Tour::Tour(vector<City *> cityList) {
+    this->cityList = cityList;
+    this->tourID = tourIDCounter++;
+}
+
+//find the fitness of the tour
+double Tour::determine_fitness() const {
+    return (1 / totalDistance()) * 1000000;
+}
+
+//calculating the total distance
+double Tour::totalDistance() const {
+    double total = 0;
+    double xComb ;
+    double yComb ;
+
+    for(vector<City>::size_type i = 1; i < cityList.size(); i++){
+        total += get_distance_between_cities( *cityList.at(i), *cityList.at(i - 1));
+    }
+    return total;
+}
+
+//get the distance between two cities
+double Tour::get_distance_between_cities(City city1, City city2) const {
+    return sqrt(pow((city1.getX() - city2.getX()),2) +
+                pow((city1.getY() - city2.getY()),2));
+}
+
+// get the distance and print and display
+void Tour::get_tour_distance() {
+    for(vector<City>::size_type i = 1; i < cityList.size(); i++){
+        cout << "Cities distance between in this tour list CityID: " << cityList.at(i)->getCityId() << " & CityID: "
+        << cityList.at(i - 1)->getCityId() << " : "
+        << get_distance_between_cities( *cityList.at(i), *cityList.at(i - 1)) << endl;
+    }
+}
+
+// shuffle the cities
+void Tour::shuffle_cities() {
+    //shuffle randomly of cityList before assign it.
+    shuffle(cityList.begin(),cityList.end(),rn);
+}
+
+//find if the city in this tour
+bool Tour::contains_city(City city) {
+    for(auto it = cityList.begin(); it != cityList.end(); it++){
+        if(it.operator*()->getCityId() == city.getCityId()){
+            return true;
+        }
+    }
+    return false;
+}
+
+//overloading the equal
+Tour &Tour::operator=(const Tour & m) {
+    swapping(*this, m);
+    return *this;
+}
+
+//helper function swap the two objects's data members
+void Tour::swapping(Tour& a, Tour b) {
+    using std::swap;
+    swap(a.cityList,a.cityList);
+    swap(a.tourID,a.tourID);
+}
+void Tour::mutateCities(int a, int b) {
+    auto tmp = *cityList.at(a);
+    *cityList.at(a) = *cityList.at(b);
+    *cityList.at(b) = tmp;
+}
+//setters and getters
+vector<City *> Tour::getCityList() const{
+    return cityList;
+}
+
+void Tour::setCityList(const vector<City *> cityList) {
+    Tour::cityList = cityList;
+}
+
+long Tour::getTourId() const {
+    return tourID;
+}
+
+//prints the list of cities in tour
+ostream &operator<<(ostream &os, const Tour &m) {
+    os << "Tour ID: " + to_string(m.tourID) + "\n";
+    os << "Fitness: " + to_string(m.determine_fitness()) + "\n";
+    for(vector<City>::size_type i = 0; i < m.cityList.size(); i++){
+        os << "City ID: " + to_string(m.cityList.at(i)->getCityId()) + " ";
+        os << "x: " +to_string(m.cityList.at(i)->getX()) + " ";
+        os << "y: " +to_string(m.cityList.at(i)->getY()) + "\n";
+    }
+    return os;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tour.hpp
@@ -0,0 +1,48 @@
+//
+// Created by jcheong on 2019-11-09.
+//
+
+#ifndef GENETICALGORITHM_TOUR_HPP
+#define GENETICALGORITHM_TOUR_HPP
+
+
+#include <vector>
+#include "City.hpp"
+const double MUTATION_RATE = 0.15;
+using namespace std;
+class Tour {
+private:
+    vector<City*> cityList;
+    long tourID;
+    //helper functions prototypes
+    void swapping(Tour& a, Tour b);
+
+public:
+    //tour ids
+    static long tourIDCounter;
+    //constructors
+    Tour() = default;
+    explicit Tour(vector<City*> cityList);
+
+    //function prototype
+    double totalDistance() const;
+    void shuffle_cities();
+    double determine_fitness() const;
+    void get_tour_distance();
+    double get_distance_between_cities(City city1, City city2) const;
+    bool contains_city(City city);
+    void mutateCities(int a, int b);
+
+    //setters and getters
+    vector<City *> getCityList() const;
+    void setCityList(vector<City *> cityList);
+    long getTourId() const;
+
+    //overloading operators
+    friend ostream &operator << (ostream &os, const Tour &m);
+    Tour& operator = (const Tour & m);
+    //destructor
+    ~Tour() = default;
+};
+
+#endif //GENETICALGORITHM_TOUR_HPP
